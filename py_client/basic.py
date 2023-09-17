0fc5ddc438103082235505281ac9b0e1e0e307b6
Jacek <jacekszwarc1@gmail.com>
2023-09-17 11:56:31 +0200

    Add Serializers

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 endpoint = "http://localhost:8000/api/"
 get_response = requests.get(endpoint, json={"product_id": 123})
 
-# print("status code: ", get_response.status_code)
-# print(get_response.json())
-print(get_response.headers)
-print(get_response.text)
+print("status code: ", get_response.status_code)
+print("response.json: ", get_response.json())
+# print(get_response.headers)
+# print(get_response.text)
