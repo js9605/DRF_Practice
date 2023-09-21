bd4454f9fa8a830339fd10cc326a872b05a7f112
Jacek <jacekszwarc1@gmail.com>
2023-09-21 15:58:33 +0200

    Token Authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,12 +1,13 @@
 import requests
 
 
+headers = {'Authorization': 'Bearer 5ecfb9c73e915f01cb343ebd35d2d2074d13dfa5'}
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
     "title": "Title for testing mixins",
     "content": "Testing mixins create"
 }
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
 
--- py_client/list.py
+++ py_client/list.py
@@ -1,8 +1,20 @@
 import requests
+from getpass import getpass
 
 
-endpoint = "http://localhost:8000/api/products/"
+auth_endpoint = "http://localhost:8000/api/auth/"
+username = input("Username: ")
+password = getpass("Password: ")
+auth_response = requests.post(auth_endpoint, json={"username": username, 'password': password})
+print(auth_response.json())
 
-get_response = requests.get(endpoint)
-print(get_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/"
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
 
