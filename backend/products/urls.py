cb9a9921108c555d4ca7bd055c520416867dd93d
Jacek <jacekszwarc1@gmail.com>
2023-09-28 15:01:36 +0200

    Custom Validations with serializer

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.urls import path
 from . import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     path('', views.product_list_create_view, name='product-list'),
     path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     path('<int:pk>/delete/', views.product_delete_view),
-    path('<int:pk>/', views.product_detail_view, name='product-detail')
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('favicon.ico', lambda request: HttpResponseNotFound()),
 ]
